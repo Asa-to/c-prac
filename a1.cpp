ea5c7485d067e24c7b3d9337c7da789432773942
Asa-to <sethuna03@gmail.com>
2020-04-30 17:00:21 +0900

    commit

--- a1.cpp
+++ a1.cpp
@@ -1,16 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main(void) {
+    cout << "Hello world" << endl;
+    return 0;
 }
--- abc122b.cpp
+++ abc122b.cpp
@@ -1,25 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(void){
-    string s;
-    cin >> s;
-    int logest = 0;
-    int sum = 0;
-    for (int j = 0; j < s.length(); j++)
-    {
-        cout << j << endl;
-        if (s[j] == 'A' || s[j] == 'C' || s[j] == 'G' || s[j] == 'T')
-        {
-            sum++;
-            cout << "yes" << endl;
-        }
-        else
-        {
-            logest = max(logest, sum);
-            sum = 0;
-        }
+int main(void) {
+  string s;
+  cin >> s;
+  int logest = 0;
+  int sum = 0;
+  for (int j = 0; j < s.length(); j++) {
+    cout << j << endl;
+    if (s[j] == 'A' || s[j] == 'C' || s[j] == 'G' || s[j] == 'T') {
+      sum++;
+      cout << "yes" << endl;
+    } else {
+      logest = max(logest, sum);
+      sum = 0;
     }
-    cout << logest << endl;
+  }
+  cout << logest << endl;
 }
