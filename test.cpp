83d8c9a4f8deb2fd64c9eb9af6350b80e072e072
Asa-to <sethuna03@gmail.com>
2020-06-11 13:18:28 +0900

    commit

--- test.cpp
+++ test.cpp
@@ -7,36 +7,52 @@ typedef long long ll;
 template<typename T> bool chmax(T &a, T b) {if(a <= b){a = b; return true;}return false;}
 template<typename T> bool chmin(T &a, T b) {if(a >= b){a = b; return true;}return false;}
 
+int W, H;
+vector<vector<bool> > seen(1005, vector<bool>(1005));
+vector<int> dw = {0, 1, 0, -1};
+vector<int> dh = {1, 0, -1, 0};
+void dfs(int h, int w, vector<vector<int> > a){
+    seen[h][w] = true;
+    rep(i, 4){
+        int nextH = h + dw[i];
+        int nextW = w + dh[i];
+        if(nextH < 0 || nextW < 0 || W <= nextW || H <= nextH)continue;
+        if(seen[nextH][nextW])continue;
+        if(!a[nextH][nextW])continue;
+        dfs(nextH, nextW, a);
+    }
+}
 
 int main(void){
-    int N, M;
-    cin >> N >> M;
-    vector<bool> seat(N);
-    for(int i = 0; i < M; i++){
-        int a, b;
-        cin >> a >> b;
-        a; b--;
-        bool frag = true;
-        int now = b;
-        for(int i = 0; i < a; i++){
-            if(seat[now]) frag = false;
-            if(now == N - 1)now = 0;
-            else now++;
+    cin >> W >> H;
+    // W × H
+    vector<vector<int> > a(H, vector<int>(W));
+    rep(i, H){
+        rep(j, W){
+            cin >> a[i][j];
         }
-        if(frag){
-            now = b;
-            for(int i = 0; i < a; i++){
-                seat[now] = true;
-                if(now == N - 1)now = 0;
-                else now++;
+    }
+    int ans = 0;
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            if(!seen[i][j] && a[i][j]){
+                dfs(i, j , a);
+                ans++;
             }
         }
     }
-    int sum = 0;
-    for(int i = 0; i < N; i++){
-        if(seat[i]){
-            sum++;
-        }
-    }
-    cout << sum << endl;
+    // for(int i = 0; i < H; i++){
+    //     for(int j = 0 ; j < W; j++){
+    //         cout << (seen[i][j] ? 1 : 0) << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "----------------------------" << endl;
+    // for(int i = 0; i < H; i++){
+    //     for(int j = 0; j < W; j++){
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    cout << ans << endl;
 }
